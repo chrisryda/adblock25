daa2d86a78be2ae01ad7b2737efa94ff0554dbc4
chrisryda <chrisryda@hotmail.com>
2024-04-04 15:55:14 +0200

    Terribly ugly, sometimes functioning first draft

--- diff.py
+++ diff.py
@@ -2,27 +2,33 @@
 def same(x, y, delta): 
     with open(x, "rb") as one: 
         with open(y, "rb") as two:
-            d = 0 
             chunk = other = True 
             while chunk or other: 
                 chunk = one.read(delta)
                 other = two.read(delta)
                 if chunk != other:
-                    d += 1
-                    print("Diff", d)
-                    chunk = process(chunk, other, int(delta/10))
-                out.write(chunk)
-
-
-def process(chunk, other, delta):
-    pass
-    
+                    print("Diff")
+                    flag = 1
+                    while flag != 0:
+                        chunk += one.read(delta)
+                        other += two.read(delta)
+                        idx = chunk.find(other[-1000:])
+                        if idx == -1:
+                            continue
+                        else:
+                            flag = 0
+                            chunk = chunk[idx:]
+                            # other = other[-1000:]
+                out.write(chunk)       
+            
+# with open("out.mp3", "wb") as out:
+#     same("/home/crd/Downloads/TheInfiniteMonkeyCage-20091130-ScienceAndComedians.mp3",
+#     "/home/crd/Downloads/TheInfiniteMonkeyCage-20091130-ScienceAndComedians(1).mp3",
+#     10_000
+#     )
     
 with open("out.mp3", "wb") as out:
-    same("/home/crd/Downloads/TheInfiniteMonkeyCage-20091130-ScienceAndComedians.mp3",
-         "/home/crd/Downloads/TheInfiniteMonkeyCage-20091130-ScienceAndComedians(1).mp3",
-         10_000
+    same("/home/crd/Downloads/GlobalNewsPodcast-20240401-GazaHospitalInRuinsAfterTwoWeekIsraeliRaid.mp3",
+    "/home/crd/Downloads/GlobalNewsPodcast-20240401-GazaHospitalInRuinsAfterTwoWeekIsraeliRaid(1).mp3",
+    10_000
     )
-
-
-# Når vanlig podcast starter igjen (ad er ferdig), må man bare finne den sequencen i andre fil 
