4d59e765381b42325cd76b9ac0a3c49c3e82a934
chrisryda <chrisryda@hotmail.com>
2024-04-22 13:26:44 +0200

    Hmmmm

---
+++ trinity.py
@@ -0,0 +1,153 @@
+import socket
+import os
+from threading import Thread
+
+class Proxy:
+    def __init__(self, port=3000):
+        self.port = port
+        self.proxy = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # self.proxy.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.proxy.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.buffer_size = 4096
+
+    def run(self):
+        # ip = input("Enter IP-address of the Waydroid-instance: ")
+        # self.proxy.bind(("ip", self.port))
+        self.proxy.bind(("192.168.240.1", self.port)) # What's up with needing this specific IP? 192.168.240.x
+        self.proxy.listen(100)
+        print("  * Proxy server is running on port {}".format(self.port))
+        while True:
+            try:
+                client, addr = self.proxy.accept()
+                print(f"\nRequest recieved => {addr[0]}:{addr[1]}")
+                req = client.recv(self.buffer_size)
+                head = self.parse_head(req)
+                print(head)
+                workers = []
+                if head["method"] == "CONNECT":
+                    try:
+                        t = Thread(target=self.serve_tunnel, args=(client, head))
+                        # t.daemon = True
+                        workers.append(t)
+                        t.start()
+                    except KeyboardInterrupt:
+                        print("Waiting for threads...", end=" ")
+                        for t in workers:
+                            t.join()
+                        print("Done")
+                else:
+                    try:
+                        t = Thread(target=self.handle_request, args=(client, req, head))
+                        # t.daemon = True
+                        workers.append(t)
+                        t.start()
+                    except KeyboardInterrupt:
+                        print("Waiting for threads...", end=" ")
+                        for t in workers:
+                            t.join()
+                        print("Done")
+            except KeyboardInterrupt:
+                print("\n\nCtrl+C pressed, exiting...")
+                print("Waiting for threads...", end=" ")
+                for t in workers:
+                    t.join()
+                print("Done")
+                os._exit(0)
+
+    def handle_request(self, client, req, head):
+        port = 80
+        print("Handling req")
+        response = self.send_to_server(head["headers"]["host"], port, req)
+        client.sendall(response)
+        print("Response sent")
+        client.close()
+
+    def send_to_server(self, host, port, data):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.connect((socket.gethostbyname(host.split(":")[0]), port))
+        server.sendall(data)
+        print("Waiting for response...")
+        res = server.recv(self.buffer_size)
+        print(f"Response recieved")
+        head = self.parse_head(res)
+        headers = head["headers"]
+        
+        if "content-length" in headers and int(headers["content-length"]) > self.buffer_size:
+            n = (int(headers["content-length"]) / self.buffer_size)
+            if n > 2: 
+                res += server.recv(int(headers["content-length"]))
+            res += server.recv(int(self.buffer_size*(n - int(n)))+1)
+        
+        # print(res)
+        server.close()
+        return res
+
+    def serve_tunnel(self, client, head):
+        host = head["headers"]["host"].split(":")[0]
+        port = int(head["headers"]["host"].split(":")[1])
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.connect((socket.gethostbyname(host), port))
+        if head["method"] == "CONNECT":
+            try:
+                reply = "HTTP/1.0 200 HTTP/1.0 200 Connection established\r\n\r\n"
+                # print(reply)
+                client.sendall(reply.encode())
+            except socket.error as err:
+                # If the connection could not be established, exit
+                # Should properly handle the exit with http error code here
+                print(err)  
+            server.setblocking(0)
+            client.setblocking(0)
+            print("Serving tunnel")
+            while True:
+                    try:
+                        request = server.recv(self.buffer_size)
+                        # print(f"Request:\n{request}")
+                        client.sendall( request )
+                    except socket.error as err:
+                        # print(err)
+                        pass
+                    try:
+                        reply = client.recv(self.buffer_size)
+                        # print(f"Reply:\n{reply}")
+                        server.sendall( reply )
+                    except socket.error as err:
+                        # print(err)
+                        pass
+                    except KeyboardInterrupt:
+                        return
+
+    def recvall(self, c, n):
+        data = b''
+        while n > 0:
+            s = c.recv(n)
+            if not s: raise EOFError
+            data += s
+            n -= len(s)
+        return data
+    
+    def parse_head(self, head_request):
+        nodes = head_request.split(b"\r\n\r\n")
+        heads = nodes[0].split(b"\r\n")
+        meta = heads.pop(0).decode("utf-8")
+        data = {
+            "method" : meta.split(" ")[0],
+            "meta": meta,
+            "headers": {},
+            "chunk": b""
+        }
+        if len(nodes) >= 2:
+            data["chunk"] = nodes[1]
+    
+        for head in heads:
+            pieces = head.split(b": ")
+            key = pieces.pop(0).decode("utf-8")
+            if key.startswith("Connection: "):
+                data["headers"][key.lower()] = "close"
+            else:
+                data["headers"][key.lower()] = b": ".join(pieces).decode("utf-8")
+        return data
+
+if __name__ == "__main__":
+    proxy = Proxy(3001)
+    proxy.run()
