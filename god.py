9c82aaa959319593733adbf58c75463b0c033667
chrisryda <chrisryda@hotmail.com>
2024-05-01 15:17:14 +0200

    New iter

--- god.py
+++ god.py
@@ -11,46 +11,58 @@ def refresh():
     except FileNotFoundError:
         pass
     
-def chunks(lst, n):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+# def chunks(lst, n):
+#     for i in range(0, len(lst), n):
+#         yield lst[i:i + n]
 
 class MitmAddon:
     def __init__(self):
         self.delta = 2**14 # 2**11 = 2048
         self.num = 0
         self.url = ""
-        self.d = b""
+        # self.d = b""
         self.accessed = False
+        
         refresh()
-    
+        
     def response(self, flow: http.HTTPFlow) -> None:
         self.num += 1
         flow.response.headers["count"] = str(self.num)
-        if flow.response.status_code == 302 and self.num == 1:
-            self.url = flow.request.url
+        try:
+            if flow.response.headers["Content-Type"] != "audio/mpeg":
+                if flow.response.status_code == 302 and self.num == 1:
+                    self.url = flow.request.url
+                return
         
-        if self.url and flow.response.status_code == 200:
+        except KeyError:
+                return
+        
+        print(f"Going for it with url: {self.url}")
+        if flow.response.status_code == 200 and self.url :
+            data = flow.response.content
+            d = self.strip_ads(data)
+
             if not self.accessed:
                 self.accessed = True
-                data = flow.response.content
-                with requests.get(self.url, stream=True) as x:
-                    with open("./tmp/one.mp3", "wb") as out: # DEL
-                        for chunk in x.iter_content(self.delta):
-                            out.write(chunk) # DEL
-                            if data.find(chunk) != -1:   
-                                self.d += chunk
-            # print("Writing content")
-            with open("./tmp/frommitm.mp3", "wb") as out:
-                out.write(flow.response.content)
-            
-            with open("./tmp/response.mp3", "wb") as out:
-                out.write(self.d)
+                with open("./tmp/frommitm.mp3", "wb") as out:
+                    out.write(flow.response.content)
+                with open("./tmp/response.mp3", "wb") as out:
+                    out.write(d)
             
             flow.response = http.Response.make(
                 200,
-                self.d,  
-                {"Content-Length": str(len(self.d))}
+                d,  
+                {"Content-Length": str(len(d))}
             )
+
+    def strip_ads(self, data: bytes) -> bytes:
+        d = b""
+        with requests.get(self.url, stream=True) as x:
+            with open("./tmp/one.mp3", "wb") as out: # DEL
+                for chunk in x.iter_content(self.delta):
+                    out.write(chunk) # DEL
+                    if data.find(chunk) != -1:   
+                        d += chunk
+        return d
 
 addons = [MitmAddon()]
