dd823bbd5a006dadf109a447686dd7f0cb7fcbd9
chrisryda <chrisryda@hotmail.com>
2024-05-02 11:36:36 +0200

    Small improv in flow

--- addon.py
+++ addon.py
@@ -4,7 +4,7 @@ import os
 from mitmproxy import http
 
 # Se på connections mot en host: f.eks megaphone.fm, og sende samme d til alle
-# Forbedre if-statement linje 27
+# Forbedre if-statement linje 27    
 
 def refresh():
     try:
@@ -18,44 +18,48 @@ class AdStripper:
     def __init__(self):
         self.delta = 2**14 # 2**11 = 2048
         self.url = ""
-        self.connections = []
+        self.accessed = False 
+        self.connections = {}
         refresh()
     
     def response(self, flow: http.HTTPFlow) -> None:
         try:
-            if flow.response.headers["Content-Type"] != "audio/mpeg":
-                if (flow.response.status_code == 302 and flow.client_conn not in self.connections):
-                    self.url = flow.request.url
-                    self.connections.append(flow.client_conn)
-                return
-
-            else:
-                if flow.response.status_code == 200 and self.url :
-                    print(f"Going for it with url: {self.url}")
-                    data = flow.response.content
-                    d = self.strip_ads(data)
-                    print("Ads stripped, sending response")
-                    flow.response = http.Response.make(
-                        200,
-                        d,  
-                        {"Content-Length": str(len(d))}
-                    )
-                    with open("./tmp/frommitm.mp3", "wb") as out:
-                        out.write(flow.response.content)
-                    with open("./tmp/response.mp3", "wb") as out:
-                        out.write(d)
-        
+            content_type = flow.response.headers["Content-Type"]
         except KeyError:
-                return
-
+            return
+        
+        if content_type != "audio/mpeg":
+            if flow.response.status_code == 302 and not self.accessed:
+                self.accessed = True
+                self.url = flow.request.url
+        
+        else:
+            if flow.response.status_code == 200 and self.url:
+                data = flow.response.content
+                print("Going in")
+                self.d = self.strip_ads(data)
+                print("Ads stripped, sending response")
+                flow.response = http.Response.make(
+                    200,
+                    self.d,  
+                    {"Content-Length": str(len(d))}
+                )
+                
+                # with open("./tmp/frommitm.mp3", "wb") as out:
+                #     out.write(flow.response.content)
+                # with open("./tmp/response.mp3", "wb") as out:
+                #     out.write(d)
+                self.accessed = False
+        
+        
     def strip_ads(self, data: bytes) -> bytes:
         d = b""
         with requests.get(self.url, stream=True) as x:
-            with open("./tmp/one.mp3", "wb") as out: # DEL
-                for chunk in x.iter_content(self.delta):
-                    out.write(chunk) # DEL
-                    if data.find(chunk) != -1:   
-                        d += chunk
+            # with open("./tmp/one.mp3", "wb") as out: # DEL
+            for chunk in x.iter_content(self.delta):
+                # out.write(chunk) # DEL
+                if data.find(chunk) != -1:   
+                    d += chunk
         return d
 
 addons = [AdStripper()]
